4620718d92bd9b70649489daa6139d992f0e2cdb
Gabriela Garcia <gmgarcia@udallas.edu>
2019-10-28 17:24:02 -0500

    HW 5 complete.

--- DatesPlayground.playground/Contents.swift
+++ DatesPlayground.playground/Contents.swift
@@ -3,14 +3,16 @@ import Foundation
 // Start with a date, like so:
 
 let date = Date()
-
 // Create a date using today's date and a DateComponents structure
+let components = DateComponents(calendar: .current, year: 2019, month: 10, day: 28)
+let today = components.date
 
 // Compare the two dates to see if they occur in the same Calendar day
-
-// Use a date formatter
+print(Calendar.current.isDate(date, inSameDayAs: today!))// Use a date formatter
 
 let formatter = DateFormatter()
 
 // use the formatter to print the day in a friendly format.
 // hint, use NSDateFormatter.com to get an appropriate printing format string.
+formatter.dateFormat = "EEEE, MMM d, yyyy"
+print(formatter.string(from: today!))
